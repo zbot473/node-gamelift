51fba39c8a715426cd1cec916053c20beba54321
Brian Rojas <marchinram@gmail.com>
2020-06-21 02:23:42 -0500

    prepublish script to convert symlinks (#3)

---
+++ bin/prepublish.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const appRoot = require('app-root-path')
+
+function convertSymlinksToFiles(dirPath) {
+  const files = fs.readdirSync(dirPath)
+  files.forEach((file) => {
+    if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+      convertSymlinksToFiles(`${dirPath}/${file}`)
+    } else {
+      const filePath = path.join(`${dirPath}`, '/', file)
+      if (fs.lstatSync(filePath).isSymbolicLink()) {
+        const origFilePath = path.join(`${dirPath}`, fs.readlinkSync(filePath))
+        fs.unlinkSync(filePath)
+        fs.copyFileSync(origFilePath, filePath)
+      }
+    }
+  })
+}
+
+convertSymlinksToFiles(path.join(__dirname, '/../gamelift/lib'))
